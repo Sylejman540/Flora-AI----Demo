12055102c795f2b75c1c6f058040d2dfd0c8fb24
Sylejman Durguti <durgutisylejman00@gmail.com>
2025-05-17 12:40:24 +0200

    Logic for the navbar

--- Main Page/index.php
+++ Main Page/index.php
@@ -10,60 +10,8 @@
 </head>
 <body>
     <!-- The Class from nav to banner section -->
-    <main class="nav-bann bg-[#004F44]">
-        <section class="flex justify-around gap-14 nav">
-            <!-- LOGO -->
-            <img src="images/logo(2).png" alt="" class="w-18 h-18 hidden md:block cursor-pointer" onclick="location.reload()">
-            <!-- For Desktop -->
-            <nav>
-                <ul class="md:flex gap-5 mt-5 text-lg text-white hidden">
-                    <li><a href="#" class="hover:text-green-300 transition duration-300">AI Flower Care</a></li>
-                    <li><a href="shop" class="hover:text-green-300 transition duration-300">Shop Flowers/Garden Tools</a></li>
-                    <li><a href="#" class="hover:text-green-300 transition duration-300">Hire a Gardener</a></li>
-                </ul>
-            </nav>
-
-            <article class="flex gap-5 mt-1">
-                <img src="images/search 1.png" alt="Search" class="w-5 h-5 mt-5 cursor-pointer">
-                <img src="images/person 1.png" alt="Person" class="w-5 h-5 mt-5 cursor-pointer">
-                <img src="images/cart 2.png" alt="Cart" class="w-6 h-5 mt-5 cursor-pointer">
-            </article>
-        </section>
-
-        <section class="flex justify-around mt-20 main">
-            <article>
-                <h1 class="text-7xl text-white header">Happiness<br>blooms from<br>within</h1>
-                <p class="text-gray-100 mt-7 text-lg">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio, ratione!<br>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
-                <div class="flex gap-8 mt-10">
-                    <button class="bg-white text-black p-2 px-5 rounded-sm hover:bg-gray-100 cursor-pointer"><a href="#shop">Shop Now</a></button>
-                    <div class="flex gap-5 mt-2">
-                        <a href="#" class="text-gray-100">Explore Our AI</a>
-                        <img src="images/arrow.png" alt="Arrow" class="w-4 h-2 mt-2.5 cursor-pointer">
-                    </div>
-                </div>
-            </article>
-            <!-- Pattern -->
-            <img src="images/pattern.png" alt="Pattern" class="absolute mt-130 ml-15">
-            <article class="flex gap-2">
-                <div>
-                    <div>
-                        <button class="bg-[#004F44] text-white rounded-sm absolute mt-3 ml-3 px-3 py-1 hover:bg-green-800 cursor-pointer">New</button>
-                        <img src="images/image 3.png" alt="Image 1" class="w-3xs h-3xs hidden md:block">
-                    </div>
-                    <div>
-                        <img src="images/image 2.png" alt="Image 2" class="w-3xs h-3xs hidden md:block relative">
-                    </div>
-                </div>
-                <div>
-                    <button class="bg-[#004F44] text-white rounded-sm absolute mt-3 ml-3 px-3 py-1 hover:bg-green-800 cursor-pointer">Featured</button>
-                    <h1 class="text-3xl text-black ml-20 absolute mt-118">Anthurium Flower</h1>
-                    <p class="text-black absolute ml-2 mt-127 text-center">Lorem ipsum dolor sit amet, consectetur adipisicing<Br> elit. Dignissimos, exercitationem.Lorem ipsum<br> dolor sit, amet consectetur<Br></p>
-                    <img src="images/dark.png" alt="Dark Image" class="w-sm absolute mt-77">
-                    <img src="images/image 1.png" alt="Image 3" class="w-sm h-157 hidden md:block">
-                </div>
-            </article>
-        </section>
-    </main>
+   
+    <?php  include_once("./components/nav_bar.php") ?>
 
     <!-- RIGHT AND LEFT IMAGES -->
     <div class="flex justify-between">
